f5db0d49694aa24930fe35bf03120ecf76eb7f1e
subhankar2506 <subhankarsen09@gmail.com>
2025-03-16 18:22:27 +0530

    Update app.py

--- deploy/app.py
+++ deploy/app.py
@@ -13,6 +13,11 @@ import datetime
 import base64
 from matplotlib.gridspec import GridSpec
 
+# Add the current directory to the path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+    
 from model import XceptionTime
 
 # Set up page configuration
